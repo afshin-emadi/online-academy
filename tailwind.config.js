6033ff233debb354c866998d9907a7bc2bed82be
Afshin Emadi <work.afshinemadi@gmail.com>
2023-10-27 09:16:34 +0330

    create first custom animation for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,15 @@ export default {
         "myPurpule-50": "#820CDF",
         "myRose-70": "#C40F50",
       },
+      keyframes: {
+        slideDown: {
+          "0%": { "transform-origin": "0 0", transform: "translateY(-40%)" },
+          "100%": { "transform-origin": "0 0", transform: "translateY(0%)" },
+        },
+      },
+      animation: {
+        slideDown: "slideDown 0.3s linear 1",
+      },
     },
   },
   plugins: [],
